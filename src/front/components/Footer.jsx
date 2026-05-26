632c26671c99a1a81f4fec4c7f7ac5a23e731283
Ana Lopez <avantadesignlab@gmail.com>
2026-05-26 23:30:53 +0000

    feat: add responsive footer section with CoralHub branding

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,11 +1,67 @@
+import logoWhite from "../assets/img/logo CoralHub_White.png";
+
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Check the <a target="_blank" href="https://4geeks.com/docs/start/react-flask-template">template documentation</a> <i className="fa-solid fa-file"></i> for help.
-		</p>
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+	<footer className="footer-section mt-5">
+		<div className="container">
+			<div className="row gy-4">
+
+				{/* BRAND */}
+				<div className="col-lg-4 footer-brand">
+					<img
+						src={logoWhite}
+						alt="CoralHub logo"
+						className="footer-logo mb-3"
+					/>
+					<p className="footer-text">
+						Buy and sell aquarium products with a modern marketplace experience.
+					</p>
+				</div>
+
+				{/* LINKS */}
+				<div className="col-6 col-lg-2">
+					<h5 className="footer-title">Explore</h5>
+					<ul className="list-unstyled">
+						<li><a href="#" className="footer-link">Home</a></li>
+						<li><a href="#" className="footer-link">Catalog</a></li>
+						<li><a href="#" className="footer-link">Dashboard</a></li>
+					</ul>
+				</div>
+
+				{/* SUPPORT */}
+				<div className="col-6 col-lg-2">
+					<h5 className="footer-title">Support</h5>
+					<ul className="list-unstyled">
+						<li><a href="#" className="footer-link">Contact</a></li>
+						<li><a href="#" className="footer-link">Help Center</a></li>
+						<li><a href="#" className="footer-link">Privacy</a></li>
+					</ul>
+				</div>
+
+				{/* NEWSLETTER */}
+				<div className="col-lg-4">
+					<h5 className="footer-title">Stay Updated</h5>
+
+					<div className="d-flex gap-2 mt-3">
+						<input
+							type="email"
+							className="form-control footer-input"
+							placeholder="Enter your email"
+						/>
+
+						<button className="btn footer-btn">
+							Subscribe
+						</button>
+					</div>
+				</div>
+
+			</div>
+
+			{/* BOTTOM */}
+			<div className="footer-bottom text-center mt-5 pt-4">
+				<p className="mb-0">
+					© 2026 CoralHub. All rights reserved.
+				</p>
+			</div>
+		</div>
 	</footer>
 );
